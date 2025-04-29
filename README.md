24655a6d1a30f89a1e253d1c32f170e5920b3b2b
AndreaVega <andreavegavalencia@gmail.com>
2025-04-29 08:34:34 -0400

    Updated Spots project with Javascript

--- README.md
+++ README.md
@@ -11,18 +11,22 @@ TripleTen SWE
 **Intro**
 
 This project is made so all the elements are displayed correctly on popular screen sizes. (From 320px - 1440px)
+User is able edit the profile by entering their name and description. They are also able to enter a custom image and image description in website.
 
 **Technologies and Techniques used**
 
 This project includes media queries, flexbox, display:grid, HTML, CSS.
-BEM Methodology, CSS and Markup validators used to check for valid code.
+BEM Methodology, JavaScript. CSS and Markup validators used to check for valid code.
 Formatted using Prettier.
 
 **Images and links**
 
 -Link to deployed project on Github Pages: https://drevega.github.io/se_project_spots/
 
--Link to video description of this project.
+-Link to video description of this project:
 https://drive.google.com/file/d/1zFe-gki1S4N1cdJoSvI9J3fdBJOi0vmn/view?usp=sharing
+
+-Link to modal opening/closing video using JavaScript:
+https://drive.google.com/file/d/17_ePeWZ78OSR2bMZ-71aOZo2wxhMH46B/view?usp=sharing
 
 _Thank you for viewing!_
--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,28 @@ const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseButton = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostButton = document.querySelector(".profile__new-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
 
+const editNewPostForm = newPostModal.querySelector(".modal__form");
+const editImageLink = newPostModal.querySelector("#card-image-input");
+const editCaptionModal = newPostModal.querySelector("#image-caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -23,3 +39,21 @@ newPostButton.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  console.log(editImageLink.value);
+  console.log(editCaptionModal.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+editNewPostForm.addEventListener("submit", handleAddCardSubmit);
